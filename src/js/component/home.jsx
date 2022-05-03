44d412254ffa8891b05df18647781c2f8546c925
josema9119 <jose.ma.mm.91@gmail.com>
2022-05-03 18:12:58 +0000

    centrado solucionado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -48,35 +48,37 @@ const Home = () => {
 	return (
 		<div className="container bg-light justify-content-center">
 			<h1 className=" mx-auto text-center">Todo´s List</h1>
-			<input
-				onChange={(e) => setTask(e.target.value)}
-				value={task}
-				type="text"
-				className="col-4 border-0 d-flex self-align-center"
-				placeholder="Task"
-				onKeyDown={(e) => {
-					if (e.key === "Enter") {
-						task !== "" && task !== " "
-							? setList([...list, task])
-							: null;
-						setTask("");
-					}
-				}}></input>
-			<ul className="list-group">
-				{list.map((task, index) => (
-					<List
-						key={index}
-						inputTask={task}
-						quit={() => deleteTask(index)}
-					/>
-				))}
+			<div className="container bg-light justify-content-center">
+				<input
+					onChange={(e) => setTask(e.target.value)}
+					value={task}
+					type="text"
+					className="col-4 border-0 d-flex self-align-center"
+					placeholder="Task"
+					onKeyDown={(e) => {
+						if (e.key === "Enter") {
+							task !== "" && task !== " "
+								? setList([...list, task])
+								: null;
+							setTask("");
+						}
+					}}></input>
+				<ul className="list-group">
+					{list.map((task, index) => (
+						<List
+							key={index}
+							inputTask={task}
+							quit={() => deleteTask(index)}
+						/>
+					))}
 
-				<span className="col-4 border-0 shadow p-3 bg-white">
-					{list.length === 0
-						? "No task, add a task"
-						: list.length + " item left"}
-				</span>
-			</ul>
+					<span className="col-4 border-0 shadow p-3 bg-white">
+						{list.length === 0
+							? "No task, add a task"
+							: list.length + " item left"}
+					</span>
+				</ul>
+			</div>
 		</div>
 	);
 };
